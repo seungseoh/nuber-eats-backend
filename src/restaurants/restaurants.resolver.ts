70d13b11e014bb79123963dd684727f19e5b4312
Simon Oh <dev.ohss@gmail.com>
2023-06-29 13:32:42 -0400

    #2.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { Restaurant } from './entities.restaurants.entity';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './restaurants.entity';
 
-@Resolver()
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
