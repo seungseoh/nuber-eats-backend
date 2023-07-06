34e94f6d82afb7d2538401acfc42fd7ff08ddcab
Simon Oh <dev.ohss@gmail.com>
2023-07-06 09:47:12 -0400

    UNIT TESTING THE USER SERVICE

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -81,21 +81,20 @@ export class UserService {
     } catch (error) {
       return {
         ok: false,
-        error,
+        error: "Can't log user in",
       };
     }
   }
 
   async findById(id: number): Promise<UserProfileOutput> {
     try {
-      const user = await this.users.findOne({ where: { id } });
-      if (user) {
-        return {
-          ok: true,
-          user: user,
-        };
-      }
+      const user = await this.users.findOneOrFail({ where: { id } });
+      return {
+        ok: true,
+        user,
+      };
     } catch (error) {
+      console.log('#######findById');
       return { ok: false, error: 'User Not Found' };
     }
   }
@@ -150,7 +149,7 @@ export class UserService {
       }
       return { ok: false, error: 'Verification not found.' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'Could not verify email.' };
     }
   }
 }
