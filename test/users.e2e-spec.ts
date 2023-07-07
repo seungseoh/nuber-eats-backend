a101dcbe8a60d2b4730418b0a94f54b9310e2207
Simon Oh <dev.ohss@gmail.com>
2023-07-07 11:28:26 -0400

    #10.2 Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
 
@@ -31,7 +32,35 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('can createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
