56c8ca148e3d7b9a9b101f60f0e86d9e673f9232
Simon Oh <dev.ohss@gmail.com>
2023-07-10 13:46:23 -0400

    #11.10 Edit Restaurant Testing

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -4,9 +4,13 @@ import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 import { CategoryRepository } from './repositories/category.repository';
+import { TypeOrmCustomModule } from 'src/db/typeorm.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
+  imports: [
+    TypeOrmModule.forFeature([Restaurant]),
+    TypeOrmCustomModule.forCustomRepository([CategoryRepository]),
+  ],
   providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
