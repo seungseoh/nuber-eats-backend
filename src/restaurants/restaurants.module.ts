dd0a0fc408f3250e965bd1c33eac427c4725ee66
Simon Oh <dev.ohss@gmail.com>
2023-07-10 13:13:53 -0400

    #11.9 Edit Restaurant part Three

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
-import { Category } from './entities/category.entity';
+import { CategoryRepository } from './repositories/category.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
   providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
