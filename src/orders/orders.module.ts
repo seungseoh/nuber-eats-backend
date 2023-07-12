c456a64e3f67d519bffb99a7904e73c2bb61eee5
Simon Oh <dev.ohss@gmail.com>
2023-07-11 21:42:11 -0400

    #12.11 Create Order part Six

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,9 +4,11 @@ import { Order } from './entities/order.entity';
 import { OrderResolver } from './orders.resolver';
 import { OrderService } from './orders.service';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { OrderItem } from './entities/order-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Restaurant])],
+  imports: [TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish])],
   providers: [OrderService, OrderResolver],
 })
 export class OrdersModule {}
