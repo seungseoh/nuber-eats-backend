2a58226375e8c9a6f549678461c3efbcd7f84d43
Simon Oh <dev.ohss@gmail.com>
2023-07-12 09:30:34 -0400

    #12.12 getOrders part One

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -3,10 +3,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from './entities/order.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Dish } from 'src/restaurants/entities/dish.entity';
 import { OrderItem } from './entities/order-item.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 
 @Injectable()
 export class OrderService {
@@ -90,6 +91,51 @@ export class OrderService {
       return {
         ok: false,
         error: 'Could not create order.',
+      };
+    }
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+      // Customer
+      if (user.role === UserRole.Client) {
+        orders = await this.orders.find({
+          where: { customer: { id: user.id } },
+        });
+        // Driver
+      } else if (user.role === UserRole.Delivery) {
+        orders = await this.orders.find({
+          where: {
+            driver: { id: user.id },
+          },
+        });
+        // Owner
+      } else if (user.role === UserRole.Owner) {
+        const restaurants = await this.restaurants.find({
+          where: {
+            owner: { id: user.id },
+          },
+          relations: ['orders'],
+        });
+        /**
+         * https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+         * flat() 메서드는 모든 하위 배열 요소를 지정한 깊이까지 재귀적으로 이어붙인 새로운 배열을 생성합니다.
+         * dept는 중첩 배열 구조를 평탄화할 때 사용할 깊이 값. 기본값은 1입니다.
+         */
+        orders = restaurants.map((restaurant) => restaurant.orders).flat(1);
+      }
+      return {
+        ok: true,
+        orders,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not get orders',
       };
     }
   }
