b8adc1b194b12f5f159ddf6cf461f3d47ee4a88b
Simon Oh <dev.ohss@gmail.com>
2023-07-13 12:00:14 -0400

    #14.3 createPayment part Two

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { PaymentResolver } from './payments.resolver';
 import { PaymentService } from './payments.service';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
   providers: [PaymentService, PaymentResolver],
 })
 export class PaymentsModule {}
--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,8 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payments.service';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  CreatePaymentOuput,
+  CreatePaymentInput,
+} from './dtos/create-payment.dto';
 
 @Resolver((of) => Payment)
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation((returns) => CreatePaymentOuput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ): Promise<CreatePaymentOuput> {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
 }
